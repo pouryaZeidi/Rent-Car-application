c0c5dbeca574d171a7f0e164197f860c55ac2d61
HP <pouryazeidi@yahoo.com>
2024-09-15 19:11:32 +0300

    about page

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -16,7 +16,7 @@ const aboutus = () => {
         backgroundPosition: 'center',
     }}
     ></div>
-    <div className='w-full h-[100vh]' style={{ 
+    <div className='w-full h-[100vh] rounded-3xl' style={{ 
         backgroundImage: " linear-gradient(to top, rgba(255,255,255,0.1), rgba(255,255,255,0.22)),url('/About3.jpg')"
         ,backgroundSize: 'cover', 
         backgroundPosition: 'center',
